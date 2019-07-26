c0ec3e0a14a9db2d7ef2ecbe38db1a46b4d6a7c4
Tito <innowhat@gmail.com>
2019-07-26 12:26:16 +0300

    Updates

---
+++ precache-manifest.5fc55af8963d4546d2949175e3869bdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "123d66a7866e7fdab9bd4eea66f167a0",
+    "url": "/vnicorn-simple-calculator/index.html"
+  },
+  {
+    "revision": "6f7584fa218cf5d4bdb3",
+    "url": "/vnicorn-simple-calculator/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "471051394f523ad03482",
+    "url": "/vnicorn-simple-calculator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6f7584fa218cf5d4bdb3",
+    "url": "/vnicorn-simple-calculator/static/js/2.d249d91e.chunk.js"
+  },
+  {
+    "revision": "471051394f523ad03482",
+    "url": "/vnicorn-simple-calculator/static/js/main.c4eade42.chunk.js"
+  },
+  {
+    "revision": "53ee3c53c157692efe08",
+    "url": "/vnicorn-simple-calculator/static/js/runtime~main.ed9f58b1.js"
+  },
+  {
+    "revision": "7efa4c78873ed651edbb5eb9659f1dad",
+    "url": "/vnicorn-simple-calculator/static/media/calculator-solid.7efa4c78.svg"
+  }
+]);
